c2d82abc6adae19f25c32e940062c634999cc1e8
Blugray <162154088+BlugrayGuyLegit@users.noreply.github.com>
2024-06-27 00:48:12 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,17 +1,22 @@
 import os
+from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from oauth2client.service_account import ServiceAccountCredentials
+from googleapiclient.http import MediaFileUpload
 
 # Configuration
 API_KEY = os.getenv('API_KEY')
 CLIENT_SECRETS_FILE = 'client_secrets.json'
 SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
 CHANNEL_ID = os.getenv('CHANNEL_ID')
+API_SERVICE_NAME = 'youtube'
+API_VERSION = 'v3'
 
-# Authentification
-credentials = ServiceAccountCredentials.from_json_keyfile_name(CLIENT_SECRETS_FILE, SCOPES)
-youtube = build('youtube', 'v3', developerKey=API_KEY, credentials=credentials)
-
+def get_authenticated_service():
+    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+        CLIENT_SECRETS_FILE, SCOPES)
+    credentials = flow.run_local_server()
+    print('Authentication successful!')
+    
 def search_videos(query, max_results=5):
     request = youtube.search().list(
         part='snippet',
